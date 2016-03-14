b0ee957666b6ee4db279655c06001e09c243e78d
hiromi <hiromi@hiromi-ubuntu.14.06>
2016-03-14 20:25:54 +0900

    add authenticate_user for payment controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314041858) do
+ActiveRecord::Schema.define(version: 20160314062525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "payments", force: :cascade do |t|
+    t.date     "payday"
+    t.integer  "amount"
+    t.integer  "kind"
+    t.text     "message"
+    t.text     "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
