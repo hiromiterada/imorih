a589e8275c0599e5321d3715fa981ec171971c5b
hiromi <hiromi@hiromi-ubuntu.14.06>
2016-03-14 14:12:07 +0900

    add devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,17 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+#  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: Rails.application.secrets.domain_name }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => Rails.application.secrets.smtp_address,
+    :port => 587,
+    :authentication => :plain,
+    :user_name => Rails.application.secrets.smtp_username,
+    :password => Rails.application.secrets.smtp_password,
+  }
+#  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
