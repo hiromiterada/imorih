b95bdd621255a5b4ebd982c36864b538079e6642
hiromi <hiromi@hiromi-ubuntu.14.06>
2016-03-15 02:44:55 +0900

    add validates for payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,7 @@
 class Payment < ActiveRecord::Base
+  validates :payday, presence: true
+  validates :amount, presence: true
+  validates :kind, presence: true
+  validates :started_at, presence: true
+  validates :ended_at, presence: true
 end
